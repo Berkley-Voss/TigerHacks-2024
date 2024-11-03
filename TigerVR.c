1db1aa634d5a88afa75c2552a8155cd1d09bbd68
Berkley Voss <bdvdkg@umsystem.edu>
2024-11-02 23:15:59 -0500

    VR submition

---
+++ TigerVR.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <math.h>
+#include <unistd.h>
+
+int main(void) {
+    printf("Virtual Reality for Cows?\n\n");
+    printf("It's a bovine-centric version of “The Matrix” that may seem far from reality. \nBut some cows in Israel and elsewhere are living in virtual reality. \nIsraeli dairy farmer Izzet Kocak purchased two virtual reality headsets for cattle from a Russian manufacturer \nafter seeing them demonstrated by the Russian entity. He was looking for ways \nto increase milk production to offset high feed prices. The headsets \ntransport indoor-housed cows virtually to a setting of calming green pastures, \nwhich is believed to give them an emotional boost. \nKocak also plays classical music in the barn while cows are wearing the headsets.\n");
+    printf("Kocak ran a mini-trial on his farm to measure the impact the headsets had on milk production. \nAfter 10 days of wearing the devices, the two test cows' milk production increased by \nabout twenty two percent, from 48.5 pounds per day to 59.5 pounds per day. \nHe said milk quality also improved. The developers reportedly collaborated with \nveterinarians in Russia to make sure the headsets were safe for cows. \nKocak is so impressed with the results that he has ordered 10 more devices. \nYou can see the Israeli dairyman demonstrate the headsets in his barn on this YouTube video.\n- Dairyherd.com\n");
+    printf("\nhttps://www.youtube.com/watch?v=EOyImm0IVPE\n");
+}
